5ecbbe646cefb1bf1c5ce328d145cc9eb0f51380
charls <chrlskrtt@gmail.com>
2024-05-21 17:18:53 +0800

    profile ye

--- src/main/java/com/example/rc_chat/Controller/ChatroomController.java
+++ src/main/java/com/example/rc_chat/Controller/ChatroomController.java
@@ -1,6 +1,31 @@
 package com.example.rc_chat.Controller;
 
-import javafx.scene.control.TextArea;
+import com.example.rc_chat.SplashScreen;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
 
 public class ChatroomController {
+    public AnchorPane apChatroom;
+    public HBox cHBox;
+
+    public void btnGoToProfileClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SplashScreen.class.getResource("Profile.fxml"));
+        AnchorPane profile_component = fxmlLoader.load();
+
+        cHBox.getChildren().remove(1);
+        cHBox.getChildren().add(profile_component);
+    }
+
+    public void btnStartChatClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(SplashScreen.class.getResource("Chatroom.fxml"));
+        AnchorPane profile_component = fxmlLoader.load();
+
+        cHBox.getChildren().remove(1);
+        cHBox.getChildren().add(profile_component);
+    }
 }
